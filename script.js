1b404bc99d71ef1fe898624e4815f158fe0161db
yugsharma4 <65899617+yugsharma4@users.noreply.github.com>
2023-02-15 20:15:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your code here
+const input = document.querySelector("input");
+const result = document.getElementById("result");
+const submit = document.getElementById("submitBtn");
+let arr = [];
+
+submit.addEventListener("click", () => {
+  const values = input.value;
+  arr = values.split(",");
+  arr = arr.map(num => Number(num));
+  arr.sort();
+   let cost = arr[0] + arr[1];
+    arr.shift();
+   arr[0] = cost;
+   
+   let first = 0;
+   let second = first + 1;
+   let third = second + 1;
+   
+   while(third < arr.length){
+   		let firstTwoSum = arr[first] + arr[second];
+        let secThirdSum = arr[second] + arr[third];
+        console.log("Inside: "+arr);
+        if(firstTwoSum < secThirdSum){
+        	cost += firstTwoSum;
+            arr.shift(1);
+            arr[0] = firstTwoSum;
+        }
+        else{
+        	cost += secThirdSum;
+            arr.pop();
+            arr[1] = secThirdSum;
+        }
+
+   }
+
+	if(arr.length != 0) cost += arr[0] + arr[1];
+	result.textContent = cost;
+});
+
+
+
+
