90998dfd15a60e33ea88c75dd67588e0237e349d
yugsharma4 <65899617+yugsharma4@users.noreply.github.com>
2023-03-09 11:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,35 +7,48 @@ const minCost =  () => {
   const values = input.value;
   arr = values.split(",");
   arr = arr.map(num => Number(num));
-  arr.sort();
-   let cost = arr[0] + arr[1];
-    arr.shift();
-   arr[0] = cost;
-   
-   let first = 0;
-   let second = first + 1;
-   let third = second + 1;
-   
-   while(third < arr.length){
-   		let firstTwoSum = arr[first] + arr[second];
-        let secThirdSum = arr[second] + arr[third];
-        console.log("Inside: "+arr);
-        if(firstTwoSum < secThirdSum){
-        	cost += firstTwoSum;
-            arr.shift(1);
-            arr[0] = firstTwoSum;
-        }
-        else{
-        	cost += secThirdSum;
-            arr.pop();
-            arr[1] = secThirdSum;
-        }
-
-   }
-
-	if(arr.length != 0) cost += arr[0] + arr[1];
-	result.textContent = cost;
+  result.innerHTML = minimumCostToConnectRopes(arr,arr.length);
 }
+
+
+function minimumCostToConnectRopes(arr, n) {
+  // Initialize a priority queue with all the rope lengths
+  let pq = new MinPriorityQueue({ priority: (rope) => rope });
+
+  for (let i = 0; i < n; i++) {
+    pq.enqueue(arr[i]);
+  }
+
+  let cost = 0;
+
+  // Keep connecting ropes until only one rope is left
+  while (pq.size() > 1) {
+    // Remove the two shortest ropes from the priority queue
+    let first = pq.dequeue().element;
+    let second = pq.dequeue().element;
+
+    // Add their lengths to the cost
+    cost += first + second;
+
+    // Add the new rope formed by connecting the two ropes to the priority queue
+    pq.enqueue(first + second);
+  }
+	
+  return cost;
+}
+
+
+let arr = [4, 2, 7, 6, 9];
+let n = arr.length;
+
+//console.log(minimumCostToConnectRopes(arr, n)); // Output: 62
+//Here, we use a priority queue (implemented using the MinPriorityQueue class from the js-priority-queue package) to keep track of the shortest ropes at any given point. We keep connecting the two shortest ropes until only one rope is left, and add the lengths of the connected ropes to the total cost. Finally, we return the total cost.
+
+
+
+
+
+
 
 
 
